9b21e5e362e857a7246cd7600a1689e855072c12
Karatoga <karatoga@qq.com>
2023-01-09 16:22:35 +0800

    dialogue

---
+++ Assets/Scripts/World/Dialogue.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    [TextArea]
+    public List<string> dialogues = new List<string>();
+    
+    public GameObject dialogueObject;
+    public TextMeshPro text;
+    
+    int cur = 0;
+    
+    void Start()
+    {
+        UpdateText();
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            cur += 1;
+            UpdateText();
+        }
+    }
+    
+    void UpdateText()
+    {
+        dialogueObject.SetActive(cur < dialogues.Count);
+        if(cur >= dialogues.Count) return;
+        text.text = dialogues[cur];
+    }
+    
+}
