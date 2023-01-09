32d063ff08ee91f5a87510fceb8529bdc2b61722
Karatoga <karatoga@qq.com>
2023-01-09 17:18:13 +0800

    add light mod interface

--- Assets/Scripts/Map/LightSource.cs
+++ Assets/Scripts/Map/LightSource.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class LightSource : MonoBehaviour
 {
-    public float intensity;
-    public int radius;
+    [SerializeField] float intensity;
+    [SerializeField] int radius;
+    
     Vector2Int coord => new Vector2Int(this.transform.position.x.RoundToInt(), this.transform.position.z.RoundToInt());
     public bool isActivated;
     
     void Awake()
     {
         
+    }
+    
+    public void ModifyIntensityAndRadius(float newIntensity, int newRadius)
+    {
+        if(isActivated)
+        {
+            MapManager.instance?.SetLightSource(this.intensity, this.radius, coord, true);
+            MapManager.instance?.SetLightSource(newIntensity, newRadius, coord);
+        }
+        this.intensity = newIntensity;
+        this.radius = newRadius;
     }
     
     void Start()
